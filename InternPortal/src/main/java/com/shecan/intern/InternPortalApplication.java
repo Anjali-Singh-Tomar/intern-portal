983d410901013c91170dd0b0223f24103c0152cf
Anjali Singh <117387439+Anjali-Singh-Tomar@users.noreply.github.com>
2025-12-05 12:49:42 +0530

    Add files via upload

---
+++ InternPortal/src/main/java/com/shecan/intern/InternPortalApplication.java
@@ -0,0 +1,13 @@
+package com.shecan.intern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InternPortalApplication.class, args);
+	}
+
+}
